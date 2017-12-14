bbaad4ca96fa88283b744aff73f6ef33531c6bea
mko98 <maxkoning@casema.nl>
2017-12-14 02:52:36 +0100

    toegevoegd neo4j en werkend gemaakt

--- api/user.routes.v1.js
+++ api/user.routes.v1.js
@@ -1,13 +1,11 @@
-//
-// ./api/v1/user.routes.v1.js
-//
 var express    = require('express');
 var routes     = express.Router();
 var mongodb    = require('../config/mongo.db');
 var Book = require('../model/book.model');
+const neo4j = require('neo4j-driver').v1;
 
-
-// GET ALL (books)
+var driver = neo4j.driver("bolt://localhost:7687", neo4j.auth.basic("neo4j", "admin"));
+var session = driver.session();
 
 routes.get('/books', function(req, res) {
     res.contentType('application/json');
@@ -24,41 +22,6 @@ routes.get('/books', function(req, res) {
         .catch((error) => res.status(401).json(error));
 });
 
-// GET ALL (authors)
-
-// routes.get('/authors', function(req, res) {
-//     res.contentType('application/json');
-//     Author.find({})
-//         .then((authors) => {
-//         if (authors.length === 0) {
-//             res.status(200).json('There are no authors');
-//         }
-//         else {
-//             res.status(200).json(authors);
-//         }
-//     })
-//         .catch((error) => res.status(401).json(error));
-// });
-//
-// // GET ALL (publishers)
-//
-// routes.get('/publishers', function(req, res) {
-//     res.contentType('application/json');
-//     Publisher.find({})
-//         .then((publishers) => {
-//         if (publishers.length === 0) {
-//             res.status(200).json('There are no publishers');
-//         }
-//         else {
-//             res.status(200).json(publishers);
-//         }
-//     })
-//         .catch((error) => res.status(401).json(error));
-// });
-
-
-// GET BY NAME (books)
-
 routes.get('/books/:_id', function(req, res) {
     res.contentType('application/json');
     Book.find({ _id: req.params._id} )
@@ -74,44 +37,57 @@ routes.get('/books/:_id', function(req, res) {
         .catch((error) => res.status(401).json(error));
 });
 
-// GET BY NAME (authors)
-
-// routes.get('/authors/:_id', function(req, res) {
-//     res.contentType('application/json');
-//     Author.find({ _id: req.params._id} )
-//         .then((authors) => {
-//             console.log();
-//             if (authors.length === 0) {
-//                 res.status(200).json('There are no authors');
-//             }
-//             else {
-//                 res.status(200).json(authors);
-//             }
-//         })
-//         .catch((error) => res.status(401).json(error));
-// });
-//
-// // GET BY NAME (publishers)
-//
-// routes.get('/publishers/:_id', function(req, res) {
-//     res.contentType('application/json');
-//     Publisher.find({ _id: req.params._id} )
-//         .then((publishers) => {
-//             console.log();
-//             if (publishers.length === 0) {
-//                 res.status(200).json('There are no publishers');
-//             }
-//             else {
-//                 res.status(200).json(publishers);
-//             }
-//         })
-//         .catch((error) => res.status(401).json(error));
-// });
-
-
-// POST (books)
-
 routes.post('/books', function(req, res) {
+  var bookTitle = req.body.title;
+  var bookLength = req.body.length;
+  var bookLanguage = req.body.language;
+  var bookIsbn = req.body.isbn;
+  var bookImageURL = req.body.imageURL;
+
+  var authorFirstName = req.body.author.firstName;
+  var authorLastName = req.body.author.lastName;
+  var authorDateOfBirth = req.body.author.dateOfBirth;
+  var authorImageURL = req.body.author.authorImageURL;
+
+  var publisherName = req.body.publisher.name;
+  var publisherAbbreviation = req.body.publisher.abbreviation;
+  var publisherLocation = req.body.publisher.location;
+  var publisherKvkNumber = req.body.publisher.kvkNumber;
+
+
+  session
+    .run("MERGE(n:Book {title:{neoTitle}, length:{neoLength}, language:{neoLanguage}, isbn:{neoISBN}, imageURL:{neoImageURL}})",
+    {neoTitle: bookTitle, neoLength: bookLength, neoLanguage: bookLanguage, neoISBN: bookIsbn, neoImageURL: bookImageURL})
+    session.close();
+
+    session
+    .run("MERGE(g:Author {firstName:{neoFirstName}, lastName:{neoLastName}, dateOfBirth:{neoDateOfBirth}, authorImageURL:{neoAuthorImageURL}}) RETURN g",
+    {neoFirstName: authorFirstName, neoLastName: authorLastName, neoDateOfBirth: authorDateOfBirth, neoAuthorImageURL: authorImageURL})
+    session.close();
+
+
+    session
+    .run("MATCH(a:Book {title:{neoTitle}}),(b:Author {firstName:{neoFirstName}}) MERGE(a)-[r:WRITTEN_BY]->(b) RETURN a,b",
+    {neoTitle: bookTitle, neoFirstName: authorFirstName})
+    session.close();
+
+
+    session
+    .run("MERGE(n:Publisher {name:{neoPublisherName}, abbreviation:{neoAbbreviation}, location:{neoLocation}, kvkNumber:{neoKvkNumber}}) RETURN n",
+    {neoPublisherName: publisherName, neoAbbreviation: publisherAbbreviation, neoLocation: publisherLocation, neoKvkNumber: publisherKvkNumber})
+    session.close();
+
+    session
+    .run("MATCH(a:Book {title:{neoTitle}}),(b:Publisher {name:{neoPublisherName}}) MERGE(a)-[r:PUBLISHED_BY]->(b) RETURN a,b",
+    {neoTitle: bookTitle, neoPublisherName: publisherName})
+    .then(function(result) {
+      res.status(200).json({"response": "BlogPost added to front page."});
+      session.close();
+    })
+    .catch((error) => {
+      res.status(400).json(error);
+    });
+
     Book.create(
         req.body,
         function(err, result) {
@@ -120,33 +96,6 @@ routes.post('/books', function(req, res) {
         console.log(result);
     });
 });
-
-// POST (authors)
-
-// routes.post('/authors', function(req, res) {
-//     Author.create(
-//         req.body,
-//         function(err, result) {
-//         if (err) return res.send(err);
-//         res.send(result);
-//         console.log(result);
-//     });
-// });
-//
-// // POST (publishers)
-//
-// routes.post('/publishers', function(req, res) {
-//     Publisher.create(
-//         req.body,
-//         function(err, result) {
-//         if (err) return res.send(err);
-//         res.send(result);
-//         console.log(result);
-//     });
-// });
-
-
-// PUT (books)
 
 routes.put('/books/:_id', function(req, res) {
     console.log(req);
@@ -161,37 +110,6 @@ routes.put('/books/:_id', function(req, res) {
         });
 });
 
-// PUT (authors)
-
-// routes.put('/authors/:_id', function(req, res) {
-//     Author.findOneAndUpdate({_id: req.params._id},
-//         req.body,
-//         {
-//           runValidators: true
-//         },
-//           function(err, result) {
-//             if (err) return res.send(err);
-//             res.send(result);
-//         });
-// });
-//
-// // PUT (publishers)
-//
-// routes.put('/publishers/:_id', function(req, res) {
-//     Publisher.findOneAndUpdate({_id: req.params._id},
-//         req.body,
-//         {
-//           runValidators: true
-//         },
-//           function(err, result) {
-//             if (err) return res.send(err);
-//             res.send(result);
-//         });
-// });
-
-
-// DELETE (books)
-
 routes.delete('/books/:_id', function(req, res) {
     Book.remove({_id: req.params._id},
         function (err, result) {
@@ -200,24 +118,36 @@ routes.delete('/books/:_id', function(req, res) {
         });
 });
 
-// DELETE (authors)
 
-// routes.delete('/authors/:_id', function(req, res) {
-//     Author.remove({_id: req.params._id},
-//         function (err, result) {
-//             if (err) return res.send(err);
-//             res.send(result);
-//         });
-// });
-//
-// // DELETE (publishers)
-//
-// routes.delete('/publishers/:_id', function(req, res) {
-//     Publisher.remove({_id: req.params._id},
-//         function (err, result) {
-//             if (err) return res.send(err);
-//             res.send(result);
-//         });
-// });
+
+routes.get('/authors/:firstName', function (req, res) {
+  var authorFirstName = req.params.firstName;
+
+  session
+    .run("MATCH (:Author { firstName:{neoAuthorFirstName}})-[:WRITTEN_BY]-(b:Book) RETURN b", {neoAuthorFirstName: authorFirstName})
+    .then(function(result) {
+      res.status(200).json(result);
+      session.close();
+    })
+    .catch((error) => {
+      res.status(400).json(error);
+    });
+});
+
+routes.get('/publishers/:name', function (req, res) {
+  var publisherName = req.params.name;
+
+  session
+    .run("MATCH (:Publisher { name:{neoPublisherName}})-[:PUBLISHED_BY]-(b:Book) RETURN b", {neoPublisherName: publisherName})
+    .then(function(result) {
+      res.status(200).json(result);
+      session.close();
+    })
+    .catch((error) => {
+      res.status(400).json(error);
+    });
+});
+
+
 
 module.exports = routes;
--- model/book.model.js
+++ model/book.model.js
@@ -68,26 +68,4 @@ const BookSchema = new Schema({
 
 const Book = mongoose.model('book', BookSchema);
 
-const book = new Book({
-    title: 'Dolfje Weerwolfje',
-    length: 176,
-    language: 'Dutch',
-    isbn: '9025845274',
-    imageURL: 'https://s.s-bol.com/imgbase0/imagebase3/large/FC/6/1/0/5/9200000046155016.jpg',
-    author: {
-        firstName: 'Paul',
-        lastName: 'van Loon',
-        dateOfBirth: '17/04/1955',
-        authorImageURL: 'http://www.dolfjeweerwolfje.nl/wp-content/uploads/2015/04/foto_paul.png'
-      },
-    publisher: {
-        name: 'Leopold',
-        abbreviation: 'LPD',
-        location: 'Amsterdam',
-        kvkNumber: '33201458'
-    }
-}); book.save();
-console.log(book);
-
-
 module.exports = Book;
